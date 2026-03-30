18e439ed7fcdcc8c5410cc599c6044326a61ec6f
Cody Richards <codyrichards14@gmail.com>
2026-03-29 21:39:36 -0500

    Initial Commit

---
+++ rep metronome/rep_metronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  rep_metronomeApp.swift
+//  rep metronome
+//
+//  Created by The Richards on 3/29/26.
+//
+
+import SwiftUI
+
+@main
+struct rep_metronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
